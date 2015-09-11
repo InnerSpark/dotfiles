41c2dc3c4fbe56f3d56a11895e55054994ce01d4
Mike Binder <mike@innersprk.com>
2015-09-10 23:26:09 -0700

    Adding binaries to install

--- linux/lamp.sh
+++ linux/lamp.sh
@@ -1,8 +1,10 @@
-echo "This script will setip a lamp based server"
+echo "This script will setup a lamp based server"
 
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
+  echo "Installing Dependancies"
+  sudo apt-get install build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
 fi
@@ -25,3 +27,51 @@ brew install bash
 # Install more recent versions of some tools
 brew tap homebrew/dupes
 brew install homebrew/dupes/grep
+
+binaries=(
+  ack
+  apache
+  boost
+  hub
+  capnp
+  cmus --with-ffmpeg
+  cowsay
+  elinks
+  ffmpeg
+  flac
+  git
+  gibo
+  google-sparsehash
+  graphicsmagick
+  hg
+  lame
+  latex2html
+  lua
+  luajit
+  mackup
+  mercurial
+  multimarkdown
+  mysql
+  ninja
+  pandoc
+  pandoc-citeproc
+  par
+  pdf2htmlex
+  python
+  ragel
+  ranger
+  reattach-to-user-namespace
+  rename
+  ruby
+  the_silver_searcher
+  task
+  tree
+  tmux
+  trash
+  wget
+  xvid
+  webkit2png
+  )
+
+echo "installing binaries..."
+brew install ${binaries[@]}
