a5305fa2ff56ddb24078505dacc256c8a16a53ab
Mike Binder <InnerSpark@users.noreply.github.com>
2017-11-08 10:43:23 -0800

    Update play.sh

--- macos/play.sh
+++ macos/play.sh
@@ -1,4 +1,1 @@
-# Reset Languages to zh
-defaults write -app "Google Chrome" AppleLanguages -array zh; open -F -a "Google Chrome";
-defaults write -app "Microsoft Outlook" AppleLanguages -array zh; open -F -a "Microsoft Outlook"
-defaults write -app "Spotify" AppleLanguages -array zh; open -F -a "Spotify"
+open -F -a "Google Chrome" "https://youtu.be/dQw4w9WgXcQ";
