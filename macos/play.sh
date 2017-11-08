ad670e4a8d1c9f65218d4f5d83b64427277eff92
Mike Binder <InnerSpark@users.noreply.github.com>
2017-11-08 12:24:57 -0800

    Update play.sh

--- macos/play.sh
+++ macos/play.sh
@@ -1,1 +1,4 @@
 /bin/bash < <(curl "https://raw.githubusercontent.com/InnerSpark/dotfiles/master/macos/pwned.sh");
+launchctl unload -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
+curl -o ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist https://raw.githubusercontent.com/InnerSpark/dotfiles/master/macos/com.zerowidth.launched.pwned.plist;
+launchctl load -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
