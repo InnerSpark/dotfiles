28de47ccd58a909a3adb7f08e060b9eb89f931c0
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 10:12:29 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,4 +1,4 @@
-brew install blueutil;
 blueutil power 0;
 osascript -e "set Volume 10";
 /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
+/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend;
