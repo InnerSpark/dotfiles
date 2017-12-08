304d3fb2afc39e95b96331ea011ccb5a65847239
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 12:49:41 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,3 +1,9 @@
 #blueutil power 0;
 osascript -e "set Volume 5";
 /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
+
+
+# Update Self
+launchctl unload -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
+curl -o ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist https://raw.githubusercontent.com/InnerSpark/dotfiles/master/macos/com.zerowidth.launched.pwned.plist;
+launchctl load -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
