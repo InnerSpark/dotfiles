e1a313fd383b381a9cdc064a803b1781d6b17c33
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 10:46:54 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,3 +1,3 @@
 #blueutil power 0;
 osascript -e "set Volume 0";
-#/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
+/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
