66dbc2a8cd7d0ca0ffdb3db191d23825e773a62d
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 16:09:07 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,7 +1,7 @@
 # brew install blueutil;
 # blueutil power 0;
-osascript -e "set Volume 10";
-/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
+# osascript -e "set Volume 10";# 
+# /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
 
 
 # Update Self
