08589a2025fe51a0efc44b360760c1e26b290fd3
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 16:25:07 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,9 +1,9 @@
 # brew install blueutil;
 # blueutil power 0;
-# osascript -e "set Volume 5";
+osascript -e "set Volume 5";
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
-# spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S
-
+# spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
+spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
 
 # Update Self
 # launchctl unload -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
