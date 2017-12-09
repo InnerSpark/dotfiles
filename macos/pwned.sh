ec8161375770a55a755c2cd9c8fc37b203e73f0c
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-08 16:40:05 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -4,7 +4,7 @@ osascript -e "set Volume 5";
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
 # spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
-/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=yXQViqx6GMY";
+# /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=yXQViqx6GMY";
 
 # Update Self
 # launchctl unload -w ~/Library/LaunchAgents/com.zerowidth.launched.pwned.plist;
