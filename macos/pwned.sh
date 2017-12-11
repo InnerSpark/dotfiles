34a8071d7d743795dc3516d116849976097a43d1
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-11 15:59:31 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -5,4 +5,4 @@
 # spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=yXQViqx6GMY";
-/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+# /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
