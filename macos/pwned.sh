263e981d244f3a89cf4d9a26359438f1836e7ff0
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-11 15:47:55 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,7 +1,7 @@
 # brew install blueutil;
-blueutil power 0;
-osascript -e "set Volume 10";
+# blueutil power 0;
+# osascript -e "set Volume 10";
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
 # spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
-/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=yXQViqx6GMY";
+# /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=yXQViqx6GMY";
