37535632195157960376b2cc382f4bfa3b9183ff
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-18 15:56:14 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,10 +1,10 @@
 # brew install blueutil;
 
 # Switch Bluetooth Off
-blueutil power 0;
+# blueutil power 0;
 
 # Set Volume
-osascript -e "set Volume 10";
+# osascript -e "set Volume 10";
 
 # Rickroll
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
@@ -14,11 +14,11 @@ osascript -e "set Volume 10";
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
 
 # hanukkah Song
-/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=8eB4PkCo4Lo";
+# /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=8eB4PkCo4Lo";
 
 # Lock Computer
-/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend;
+# /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend;
 
 # Stress Test
-yes "I like fan noise";
+# yes "I like fan noise";
 # killall yes;
