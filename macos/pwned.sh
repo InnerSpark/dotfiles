b5ff239f96468cb6dc4da190a2285373868fefa5
Mike Binder <InnerSpark@users.noreply.github.com>
2017-12-18 14:41:47 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,8 +1,17 @@
 # brew install blueutil;
-# blueutil power 0;
-# osascript -e "set Volume 10";
+
+# Switch Bluetooth Off
+blueutil power 0;
+
+# Set Volume
+osascript -e "set Volume 10";
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
 # spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=yXQViqx6GMY";
-# /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
+
+# hanukkah Song
+/usr/bin/open -F -a "/Applications/Google Chrome.app" "https://www.youtube.com/watch?v=8eB4PkCo4Lo";
+
+# Lock Computer
+/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend
