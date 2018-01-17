1ad600f33b2f113243633a16b457219585fdb409
Mike Binder <InnerSpark@users.noreply.github.com>
2018-01-16 16:54:15 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -1,7 +1,7 @@
 # brew install blueutil;
 
 # Switch Bluetooth Off
-# blueutil power 0;
+blueutil power 0;
 
 # Set Volume
 # osascript -e "set Volume 10";
@@ -10,7 +10,7 @@
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
 
 #Spotify Play
-# spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
+spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
 
 # hanukkah Song
