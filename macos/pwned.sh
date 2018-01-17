84877bcadf587c25ef8fd35c91caceb4d108af0f
Mike Binder <InnerSpark@users.noreply.github.com>
2018-01-16 16:56:57 -0800

    Update pwned.sh

--- macos/pwned.sh
+++ macos/pwned.sh
@@ -10,7 +10,7 @@
 # /usr/bin/open -F -a "/Applications/Google Chrome.app" "https://youtu.be/dQw4w9WgXcQ";
 
 #Spotify Play
-spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
+# spotify play spotify:album:3DMxDmUVkYDVi0LpQyuz4S;
 # spotify play spotify:track:28fuXrmmF9dYWx25dMW9dP;
 
 # hanukkah Song
