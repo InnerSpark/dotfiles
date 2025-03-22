c3bb1bbd9ff0439f9ed33d678b53942096d1eccb
Mike Binder <mike@innersparkmedia.com>
2025-03-22 08:39:13 -0700

    Adding zsh setup for Linux

---
+++ linux/setup.sh
@@ -0,0 +1,19 @@
+echo "This script will setup a lamp based server"
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check and update packages
+apt update && apt Upgrade
+
+# Install and setup .zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Download Plugins for autosuggestion and syntax highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
